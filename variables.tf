9bdc04c579803e97b4978389825ad228a9c72d35
Santiago Marulanda <santiagomm1997@gmail.com>
2023-06-25 23:35:49 +0200

    kubernetes module comple

---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "env_name" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "ms_namespace" {
+  type    = string
+  default = "microservices"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "cluster_subnet_ids" {
+  type = list(string)
+}
+
+variable "nodegroup_subnet_ids" {
+  type = list(string)
+}
+
+variable "nodegroup_desired_size" {
+  type    = number
+  default = 1
+}
+
+variable "nodegroup_min_size" {
+  type    = number
+  default = 1
+}
+
+variable "nodegroup_max_size" {
+  type    = number
+  default = 1
+}
+
+variable "nodegroup_disk_size" {
+  type = string
+}
+
+variable "nodegroup_instance_type" {
+  type = list(string)
+}
